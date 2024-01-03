25b6ce5fb515b22d3ae7365f5e10ef7a4a694da0
Your Name <you@example.com>
2024-01-03 11:16:18 +0530

    initial commit

---
+++ src/main/java/com/deployment/DeploymentApplication.java
@@ -0,0 +1,19 @@
+package com.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DeploymentApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DeploymentApplication.class);
+	}
+}
